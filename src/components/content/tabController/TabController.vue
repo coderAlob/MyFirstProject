aa7a5f35e7cf7022c1567e3eecc0112228306e7f
Alob <1649327177@qq.com>
2021-05-19 19:14:55 +0800

    ''

--- src/components/content/tabController/TabController.vue
+++ src/components/content/tabController/TabController.vue
@@ -26,6 +26,7 @@
     methods: {
       itemClick (index){
         this.currentIndex = index;
+        this.$emit('tabClick',index)
       }
     }
   }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,61 +4,9 @@
     <home-swiper :banners="banners"/>
     <home-recommend-view :recommends="recommends"/>
     <feature-view/>
-    <tab-controller class="tab-control" :titles="['流行', '新款', '精选']"/>
+    <tab-controller class="tab-control" :titles="['流行', '新款', '精选']" @tabClick="tabClick"/>
+    <goods-list :goods="showGoods"/>
 
-
-    <ul>
-      <li>列表1</li>
-      <li>列表2</li>
-      <li>列表3</li>
-      <li>列表4</li>
-      <li>列表5</li>
-      <li>列表6</li>
-      <li>列表7</li>
-      <li>列表8</li>
-      <li>列表9</li>
-      <li>列表10</li>
-      <li>列表11</li>
-      <li>列表12</li>
-      <li>列表13</li>
-      <li>列表14</li>
-      <li>列表15</li>
-      <li>列表16</li>
-      <li>列表17</li>
-      <li>列表18</li>
-      <li>列表19</li>
-      <li>列表20</li>
-      <li>列表21</li>
-      <li>列表22</li>
-      <li>列表23</li>
-      <li>列表24</li>
-      <li>列表25</li>
-      <li>列表26</li>
-      <li>列表27</li>
-      <li>列表28</li>
-      <li>列表29</li>
-      <li>列表30</li>
-      <li>列表31</li>
-      <li>列表32</li>
-      <li>列表33</li>
-      <li>列表34</li>
-      <li>列表35</li>
-      <li>列表36</li>
-      <li>列表37</li>
-      <li>列表38</li>
-      <li>列表39</li>
-      <li>列表40</li>
-      <li>列表41</li>
-      <li>列表42</li>
-      <li>列表43</li>
-      <li>列表44</li>
-      <li>列表45</li>
-      <li>列表46</li>
-      <li>列表47</li>
-      <li>列表48</li>
-      <li>列表49</li>
-      <li>列表50</li>
-    </ul>
   </div>
 </template>
 
@@ -68,8 +16,9 @@
   import HomeRecommendView from "./childComponents/HomeRecommendView";
   import FeatureView from "./childComponents/FeatureView";
   import TabController from "components/content/tabController/TabController";
+  import GoodsList from "components/content/goods/GoodsList";
 
-  import {getHomeMultidata} from "network/home";
+  import {getHomeMultidata,getHomeGoods} from "network/home";
 
   export default {
     name: "Home",
@@ -77,6 +26,17 @@
       return {
         banners: [],
         recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []},
+        },
+        currentType: 'pop'
+      }
+    },
+    computed: {
+      showGoods() {
+        return this.goods[this.currentType].list
       }
     },
     components: {
@@ -85,13 +45,56 @@
       HomeRecommendView,
       FeatureView,
       TabController,
+      GoodsList,
     },
     created() {
-      //  1.请求多个数据
-      getHomeMultidata().then(res =>{
-        this.banners = res.data.data.banner.list
-        this.recommends = res.data.data.recommend.list
-      })
+      //  1.请求轮播图数据
+      this.getHomeMultidata()
+      //  2.请求商品数据
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    methods: {
+      /*
+      * 网络请求相关方法
+      * */
+      getHomeMultidata (){
+        getHomeMultidata().then(res =>{
+          this.banners = res.data.data.banner.list
+          this.recommends = res.data.data.recommend.list
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page +1
+        getHomeGoods(type, page).then(res => {
+          this.goods[type].list.push(...res.data.data.list)
+          this.goods[type].page +=1;
+        })
+      },
+
+      /*
+      * 事件监听相关的方法
+      * */
+      tabClick(index) {
+        switch (index) {
+          case 0: {
+            this.currentType = 'pop'
+            break
+          }
+          case 1: {
+            this.currentType = 'new'
+            break
+          }
+          case 2: {
+            this.currentType = 'sell'
+            break
+          }
+          default : {
+            this.currentType = 'pop'
+          }
+        }
+      }
     }
   }
 </script>
@@ -116,5 +119,6 @@
     /*ie浏览器目前不兼容该属性*/
     position: sticky;
     top: 44px;
+    z-index: 8;
   }
 </style>
